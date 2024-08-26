0be87584c2ccb01f4fdcfc07f5bf6cbc2457422a
つくも <92912896+Thukumo@users.noreply.github.com>
2024-08-26 11:59:50 +0900

    len(l)==10でないときに対応できてなかった

--- iterfunc/iter_func.py
+++ iterfunc/iter_func.py
@@ -28,8 +28,10 @@ class iter_func:
     def __init__(self, func, inputstr, l="0123456789", sp_char="?"):
         self.func, self.inputstr, self.l, self.sp_char = func, inputstr, l, sp_char
         self.n, self.ln, self.num = inputstr.count(sp_char), len(l), 0
-        self.finum = self.ln**self.n
+        #self.finum = self.ln**self.n
         self.spstr = inputstr.split(sp_char)
+        self.stnum = [0 for _ in range(self.n)]
+        self.stnum[0] = -1
 
     def __iter__(self):
         return self
@@ -38,10 +40,16 @@ class iter_func:
         return self.finum
 
     def __next__(self):
-        if self.num == self.finum: raise StopIteration()
-        stnum = "".join(["0" for _ in range(self.n-len(str(self.num)))])+str(self.num)
-        self.num += 1
-        return self.func(generate_string(self.inputstr, [self.l[int(stnum[i])] for i in range(self.n)], self.sp_char))
+        #if self.num == self.finum: raise StopIteration()
+        #stnum = "".join(["0" for _ in range(self.n-len(str(self.num)))])+str(self.num)
+        #self.num += 1
+        if all(i == self.ln-1 for i in self.stnum): raise StopIteration()
+        self.stnum[0] += 1
+        for i in range(self.n-1):
+            if self.stnum[i] == self.ln:
+                self.stnum[i] = 0
+                self.stnum[i+1] += 1
+        return self.func(generate_string(self.inputstr, [self.l[self.stnum[i]] for i in range(self.n)], self.sp_char))
 
 def generate_string(s, l, sp_char="?"):
     """
@@ -59,11 +67,8 @@ def generate_string(s, l, sp_char="?"):
         str: The generated string with elements from `l` inserted at the appropriate positions.
 
     Example:
-        >>> generate_string("Hello?world?", ["beautiful ", "cruel "])
-        'Hellobeautiful cruel world'
+        >>> generate_string("Hello ? ? world", ["beautiful", "cruel"])
+        'Hello beautiful cruel world'
     """
-    if len(l)+1 != len(sps := s.split(sp_char)):
-        raise ValueError(f"Length of l must be equal to the number of sp_char('{sp_char}') in s.")
-    return "".join(sps[i] + l[i] for i in range(len(l))) + sps[-1]
-    if len(l)+1 != len(sps := s.split(sp_char)): raise ValueError(f"Length of l must be equal to the number of sp_char('{sp_char}') in s.") #例外の種類これでいい？
+    if len(l)+1 != len(sps := s.split(sp_char)): raise ValueError(f"Length of l must be equal to the number of sp_char('{sp_char}') in s.")
     return "".join(sps[i] + l[i] for i in range(len(l)))+sps[-1]
