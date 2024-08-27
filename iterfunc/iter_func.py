d5baf2c66f45994ca48d8ee12336509bf01ad0a8
つくも <92912896+Thukumo@users.noreply.github.com>
2024-08-27 21:14:34 +0900

    イテレータの要素(?)数を正しく返すようにした...はず

--- iterfunc/iter_func.py
+++ iterfunc/iter_func.py
@@ -2,7 +2,7 @@ class iter_func:
     """
     An iterator class that generates strings by replacing wildcard characters in a given input string.
     Args:
-        func (function): The function to be applied to each generated string.
+        func (callable): The function to be applied to each generated string.
         inputstr (str): The input string containing wildcard characters to be replaced.
         l (str, optional): The list of replacement characters. Defaults to "0123456789".
         begin_at (str, optional): The string to be started from. Defaults to None.
@@ -10,21 +10,27 @@ class iter_func:
     Raises:
         ValueError: If the length of the list of replacement characters is zero.
     Attributes:
-        __func (function): The function to be applied to each generated string.
+        __func (callable): The function to be applied to each generated string.
         __inputstr (str): The input string containing wildcard characters to be replaced.
         __l (str): The list of replacement characters.
         __sp_char (str): The wildcard character.
-        __n (int): The number of wildcard characters in the input string.
+        __n (int): The number of wildcard positions in the input string.
         __ln (int): The length of the list of replacement characters.
         __finum (int): The total number of possible combinations.
         __spstr (list): The list of substrings between wildcard characters.
         __lspstr (int): The length of the list of substrings.
         __stnum (list): The current state of each wildcard position.
+        __offset (int): The offset to adjust the iteration range.
     Methods:
         __iter__(): Returns the iterator object itself.
-        __len__(): Returns the total number of possible combinations.
-        __next__(): Generates the next string by replacing wildcard characters.
-        __gen_string(l): Generates the string by replacing wildcard characters with the given list of replacement characters.
+        __len__(): Returns the number of remaining iterations.
+        __next__(): Generates the next string in the iteration.
+    Example:
+        # Create an iter_func object
+        it = iter_func(lambda x: x.upper(), "a?b?c")
+        # Iterate over the generated strings
+        for s in it:
+            print(s)  # Output: "A0B0C", "A0B1C", ..., "A9B8C", "A9B9C"
     """
     def __init__(self, func, inputstr, l="0123456789", begin_at: str = None, sp_char="?"):
         self.__func, self.__inputstr, self.__l, self.__sp_char = func, inputstr, l, sp_char
@@ -35,16 +41,19 @@ class iter_func:
         if self.__ln == 0: raise ValueError("Length of the list of replacement character(s) must not equal zero.")
         self.__stnum = [0 for _ in range(self.__n)]
         self.__stnum[0] = -1
+        self.__offset = 0
         if begin_at != None:
             for num, i in enumerate([i for i in range(len(inputstr)) if inputstr[i] == sp_char]):
                 self.__stnum[self.__n-num-1] = l.index(begin_at[i])
             self.__stnum[0] -= 1
+            self.__offset = sum(n*self.__ln**ind for ind, n in enumerate(self.__stnum))+1
+        
 
     def __iter__(self):
         return self
 
     def __len__(self):
-        return self.__finum
+        return self.__finum - self.__offset
 
     def __next__(self):
         if all(i == self.__ln-1 for i in self.__stnum): raise StopIteration()
