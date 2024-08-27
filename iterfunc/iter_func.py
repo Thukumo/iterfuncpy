7ecde486b87e7415fbdb6c503e97e75dadbf3cbb
つくも <92912896+Thukumo@users.noreply.github.com>
2024-08-27 20:52:41 +0900

    新機能

--- iterfunc/iter_func.py
+++ iterfunc/iter_func.py
@@ -1,27 +1,32 @@
 class iter_func:
     """
-    A class that generates an iterator for a given function and input string.
+    An iterator class that generates strings by replacing wildcard characters in a given input string.
     Args:
         func (function): The function to be applied to each generated string.
-        inputstr (str): The input string containing placeholders.
-        l (str, optional): The characters to be used for replacement. Defaults to "0123456789".
-        sp_char (str, optional): The placeholder character. Defaults to "?".
-    Methods:
-        __iter__(): Returns the iterator object.
-        __len__(): Returns the total number of iterations.
-        __next__(): Returns the next generated string.
+        inputstr (str): The input string containing wildcard characters to be replaced.
+        l (str, optional): The list of replacement characters. Defaults to "0123456789".
+        begin_at (str, optional): The string to be started from. Defaults to None.
+        sp_char (str, optional): The wildcard character. Defaults to "?".
     Raises:
-        StopIteration: Raised when there are no more iterations.
-    Example:
-        # Create an instance of iter_func
-        iterator = iter_func(lambda x: x, "abc?def")
-        # Iterate over the generated strings
-        for string in iterator:
-            print(string) # abc0def, abc1def, ..., abc9def
-        # This is same as
-        for _ in iter_func(print, "abc?def"): pass
+        ValueError: If the length of the list of replacement characters is zero.
+    Attributes:
+        __func (function): The function to be applied to each generated string.
+        __inputstr (str): The input string containing wildcard characters to be replaced.
+        __l (str): The list of replacement characters.
+        __sp_char (str): The wildcard character.
+        __n (int): The number of wildcard characters in the input string.
+        __ln (int): The length of the list of replacement characters.
+        __finum (int): The total number of possible combinations.
+        __spstr (list): The list of substrings between wildcard characters.
+        __lspstr (int): The length of the list of substrings.
+        __stnum (list): The current state of each wildcard position.
+    Methods:
+        __iter__(): Returns the iterator object itself.
+        __len__(): Returns the total number of possible combinations.
+        __next__(): Generates the next string by replacing wildcard characters.
+        __gen_string(l): Generates the string by replacing wildcard characters with the given list of replacement characters.
     """
-    def __init__(self, func, inputstr, l="0123456789", sp_char="?"):
+    def __init__(self, func, inputstr, l="0123456789", begin_at: str = None, sp_char="?"):
         self.__func, self.__inputstr, self.__l, self.__sp_char = func, inputstr, l, sp_char
         self.__n, self.__ln = inputstr.count(sp_char), len(l)
         self.__finum = self.__ln**self.__n
@@ -30,6 +35,10 @@ class iter_func:
         if self.__ln == 0: raise ValueError("Length of the list of replacement character(s) must not equal zero.")
         self.__stnum = [0 for _ in range(self.__n)]
         self.__stnum[0] = -1
+        if begin_at != None:
+            for i in [i for i in range(len(inputstr)) if inputstr[i] == sp_char]:
+                self.__stnum[self.n-i-1] = l.index(begin_at[i])
+            self.__stnum[0] -= 1
 
     def __iter__(self):
         return self
